c1fd8899f832931e8c009e19d6b06939504fcaa5
Barannikov Andrey <isip_a.a.barannikov@mpt.ru>
2024-03-27 23:43:36 +0300

    New pipeline

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ def print_hi(name):
     print(f'Hi, {name}')
 
 if __name__ == '__main__':
-    print_hi('PyCharm ggrtgertg rgrtg ert tgrgertgrtg rtgwgert rhwg rtgrwtgegtgr')
+    print_hi('PyCharm')
 
